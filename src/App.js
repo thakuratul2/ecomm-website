1bf06baa822cc53606744ac06f208f83ab08b40d
thakuratul2 <pratapsinghatul111@gmail.com>
2023-10-01 10:52:53 +0530

    Work Start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+import {Header} from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+    <Header/>
       <h2>Hello World</h2>
     </div>
   );
