9f72dae60fecb6d6ca71549d5ba0a491776e7832
thakuratul2 <pratapsinghatul111@gmail.com>
2023-10-02 10:11:30 +0530

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,6 +12,7 @@ const Header = () => {
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
