bf50165f8eb9266df35a912cd536e92c7c857d0b
thakuratul2 <pratapsinghatul111@gmail.com>
2023-10-05 21:06:49 +0530

    Fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import {Header} from './components/Header';
+import Header from './components/Header';
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import AddProduct from './pages/AddProduct';
+import UpdateProduct from './pages/UpdateProduct';
 
 function App() {
   return (
     <div className="App">
+    <BrowserRouter>
     <Header/>
       <h2>Hello World</h2>
+    
+    </BrowserRouter>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,20 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 const Header = () => {
   return (
     <div>
-      <Navbar expand="lg" className="bg-body-tertiary">
-     
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+     <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto nav_bar">
+            <Link to="/add">Add Product</Link>
+            <Link to="/update">Update Product</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
           </Nav>
-        </Navbar.Collapse>
-      
-    </Navbar>
+        </Container>
+      </Navbar>
     </div>
   );
 }
